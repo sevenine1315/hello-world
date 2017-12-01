fd7e374afdf40f27df86544c944a1ae32b8729db
sevenine1315 <34129361+sevenine1315@users.noreply.github.com>
2017-12-01 09:57:06 +0800

    Add Huawei's decription
    Add Huawei

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello world!I am here...\n");
+    //printf("Hello world!I am here...\n");
+    printf("Huawei has been the world's third largest samrtpone manufacturer following Samsung from South Korea and Apple from the United States
+           since 2015\n");
     return 0;
 }
